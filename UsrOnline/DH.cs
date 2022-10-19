2e50369362260a9a63777b147e355f94e0bd1ff0
Andrew Cavallo <65136929+Awire9966@users.noreply.github.com>
2022-10-19 18:34:50 -0400

    Add files via upload

---
+++ UsrOnline/DH.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Discord.Webhook
+{
+    public static class Webhook
+    {
+
+
+
+
+
+
+
+        public static void SendMessage(string webhook, string message, string profilepictureurl, string Username)
+        {
+
+
+
+            NameValueCollection discordValues = new NameValueCollection();
+            WebClient dWebClient = new WebClient();
+            discordValues.Add("username", Username);
+            discordValues.Add("avatar_url", profilepictureurl);
+            discordValues.Add("content", message);
+
+            dWebClient.UploadValues(webhook, discordValues);
+
+
+
+        }
+    }
+}
---
+++ UsrOnline/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Threading;
+using Discord.Webhook;
+namespace UsrOnline
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+            string cd = Environment.CurrentDirectory + @"\";
+            Console.WriteLine("Loading Config.....");
+            string[] Config = File.ReadAllLines(cd + @"main.config");
+            Console.WriteLine("Config Loaded!");
+            Console.WriteLine("Creating WebClient");
+            WebClient wc = new WebClient();
+            Console.WriteLine("WebClient Created!");
+            Console.WriteLine("Starting Process...");
+            while (true)
+            {
+               string data = wc.DownloadString(Config[0]);
+                if(data.Contains(Config[1]))
+                {
+                    Webhook.SendMessage(Config[2],Config[3].Replace("{data}", data), "https://cdn-icons-png.flaticon.com/512/1827/1827301.png",Config[4]);
+                }
+
+
+                Thread.Sleep(int.Parse(Config[5]) * 60000);
+            }
+        }
+    }
+}
